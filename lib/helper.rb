9a0d7a4f765aef97b7262ab4a13608159585e024
Constantin Scholl <constantin@scholl.im>
2015-06-17 14:28:35 +0200

    Create helper file to read partitions

---
+++ lib/helper.rb
@@ -0,0 +1,9 @@
+def read_partitions_from_file(file_name)
+  partitions = []
+  model_file = File.new(file_name)
+  model_file.each do |line|
+    partitions.push(line.match(/(\d+)-(\d+)/).captures.map(&:to_i))
+  end
+  model_file.close
+  return partitions
+end
