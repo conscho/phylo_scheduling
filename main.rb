41f0dcf80b0010067356e675ef7622a9636ce604
Constantin Scholl <constantin@scholl.im>
2015-06-17 15:23:38 +0200

    Bulk reading of trees

--- main.rb
+++ main.rb
@@ -3,12 +3,13 @@ require './lib/newick'
 require './lib/helper'
 
 
+=begin
 # Get data
-tree = NewickTree.fromFile("./data/test/test.tree")
-partitions = read_partitions_from_file("./data/test/test.model")
+tree = NewickTree.fromFile("./data/medium/medium.tree")
+partitions = read_partitions_from_file("./data/medium/medium.model")
 
 # Root tree
-root = "Seq5"
+root = "241"
 print("Rooted at #{root}: ")
 tree = tree.reroot(tree.findNode(root))
 print(tree.to_s + "\n")
@@ -16,45 +17,48 @@ print(tree.to_s + "\n")
 # Results for each partition
 partitions.each do |partition|
   print("Results for partition #{partition}:\n")
-  tree_partition = tree.read_phylip("./data/test/test.phy", partition)
+  tree_partition = tree.read_phylip("./data/medium/medium.phy", partition)
   operations = tree_partition.ml_operations
   print("  Maximum Operations: " + operations[0].to_s + "\n")
   print("  Operations (without unique sites and repeats): " + operations[1].to_s + "\n")
   print("  Ratio: " + ((operations[1].to_f / operations[0].to_f) * 100).round(2).to_s + "% \n")
 end
+=end
 
 
 
-=begin
+maximum_operations = []
+operations = []
+ratio = []
+partitions_length = 0
 
-unroot_calc = [] # Unrooted operations without subtree repeat optimization
-unroot_calc_optim = []  # Unrooted operations with subtree repeat optimization
-midpoint_calc = []
-midpoint_calc_optim = []
+Dir.glob('./data/medium/random_trees/*') do |file|
 
-Dir.glob('./data/medium/*.tree') do |file|
+  # Get data
   print("Processing file: #{file}\n")
   tree = NewickTree.fromFile(file)
-  tree = tree.read_phylip('./data/medium/medium.phy')
+  partitions = read_partitions_from_file("./data/medium/medium.model")
+  partitions_length = partitions.count
 
-  result = tree.ml_calculations
-  unroot_calc.push(result[0])
-  unroot_calc_optim.push(result[1])
+  # Root tree
+  root = "241"
+  print("Rooted at #{root}: ")
+  tree = tree.reroot(tree.findNode(root))
+  print(tree.to_s + "\n")
 
-  # tree = tree.set_edge_length only if the supplied tree does not include edge lengths
-  tree = tree.midpointRoot
-  result = tree.ml_calculations
-  midpoint_calc.push(result[0])
-  midpoint_calc_optim.push(result[1])
+
+  partitions.each do |partition|
+    tree_partition = tree.read_phylip("./data/medium/medium.phy", partition)
+
+    result = tree_partition.ml_operations
+    maximum_operations.push(result[0])
+    operations.push(result[1])
+    ratio.push(((result[1].to_f / result[0].to_f) * 100).round(2))
+  end
 end
 
-print("Unrooted trees:\n")
-print("w/  subtree repeat optimization: # of random trees: #{unroot_calc.count}, mean: #{unroot_calc.mean}, variance: #{unroot_calc.variance}, standard deviation: #{unroot_calc.standard_deviation.round(2)}\n")
-print("w/o subtree repeat optimization: # of random trees: #{unroot_calc_optim.count}, mean: #{unroot_calc_optim.mean}, variance: #{unroot_calc_optim.variance}, standard deviation: #{unroot_calc_optim.standard_deviation.round(2)}\n")
+print("#{maximum_operations.count / partitions_length} rooted trees with #{partitions_length} partitions:\n")
+print("  Maximum Operations: mean: #{maximum_operations.mean}, variance: #{maximum_operations.variance}, standard deviation: #{maximum_operations.standard_deviation.round(2)}\n")
+print("  Operations (without unique sites and repeats): mean: #{operations.mean}, variance: #{operations.variance}, standard deviation: #{operations.standard_deviation.round(2)}\n")
+print("  Ratio: mean: #{ratio.mean}, variance: #{ratio.variance}, standard deviation: #{ratio.standard_deviation.round(2)}\n")
 
-print("Midpoint rooted trees:\n")
-print("w/  subtree repeat optimization: # of random trees: #{midpoint_calc.count}, mean: #{midpoint_calc.mean}, variance: #{midpoint_calc.variance}, standard deviation: #{midpoint_calc.standard_deviation.round(2)}\n")
-print("w/o subtree repeat optimization: # of random trees: #{midpoint_calc_optim.count}, mean: #{midpoint_calc_optim.mean}, variance: #{midpoint_calc_optim.variance}, standard deviation: #{midpoint_calc_optim.standard_deviation.round(2)}\n")
-
-
-=end
