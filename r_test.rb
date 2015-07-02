915a33f406c1429af072ab302138edc8fd23f5a4
Constantin Scholl <constantin@scholl.im>
2015-07-02 19:36:22 +0200

    Update R test file

--- r_test.rb
+++ r_test.rb
@@ -1,22 +1,37 @@
 require 'rinruby'
 
-my_array = [3, 5, 2, 3, 6, 7, 4, 5, 6, 2]
+R.image_path = "sample1.png"
 
-R.image_path = "sample2.png"
-R.image_path2 = "sample3.png"
-R.numbers = my_array
+my_array1 = [1, 10, 3]
+my_array2 = [1, 4, 2, 4, 3]
+my_array3 = [1, 4, 2, 5, 3, 8, 5, 2, 4, 3]
 
-R.eval("png(filename=image_path)")
-R.eval("boxplot(numbers, main='Number Array')")
-R.eval("dev.off()")
+R.numbers1 = my_array1
+R.numbers2 = my_array2
+R.numbers3 = my_array3
+R.index1 = 1
+R.index2 = 2
+R.index3 = 3
 
 R.eval <<EOF
-     png(filename="image_path2.png")
-     data<-data.frame(Stat11=numbers,
-      Stat21=numbers,
-      Stat31=rnorm(100,mean=6,sd=0.5),
-      Stat41=rnorm(100,mean=10,sd=0.5),
-      Stat12=rnorm(100,mean=4,sd=2))
-     boxplot(data)
-    dev.off()
+dataList = list()
+dataList = c(dataList, list(numbers1))
+dataList = c(dataList, list(numbers2))
+dataList = c(dataList, list(numbers3))
+
+png(filename=image_path)
+boxplot(dataList, main=toupper("Comparison of trees from one dataset"),
+        xlab="Type of trees for the dataset", ylab="Ratio of computational savings")
+dev.off()
+EOF
+
+
+R.eval <<EOF
+       png(filename="sample2.png")
+       data<-data.frame(
+         Stat31=rnorm(10,mean=6,sd=0.5),
+         Stat41=rnorm(20,mean=10,sd=0.5),
+         Stat12=rnorm(100,mean=4,sd=2))
+       boxplot(data)
+       dev.off()
 EOF
