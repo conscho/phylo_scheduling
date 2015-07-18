ec4cf470c98bd3e588632bcc9cbb428849679c6e
Constantin Scholl <constantin@scholl.im>
2015-07-18 19:35:49 +0200

    Create graph for site dependency

--- site_dependency.R
+++ site_dependency.R
@@ -17,17 +17,19 @@ for (parameter.file in files) {
                           "Number of processes:", programParameters[1, "number_of_processes"], sep = " ")
   ggplotTheme = theme(plot.margin = unit(c(1,1,1,1), "lines"), plot.title = element_text(size = rel(0.9)))
   
-  # Read Data, aggregate and subset
+  # Read and aggregate data
   rawData = read.csv(paste(programParameters[1, "data_file"]))
+  aggregatedData <- summarise(group_by(rawData, batch, tree, partition, dependency), count = n())
+  cumData = mutate(group_by(aggregatedData, batch, tree, partition), cumsum=cumsum(count), proportion=cumsum/sum(count))
+  labelData = filter(cumData, dependency == 1)
   
   # Generate graphs
   ggplotTitle = ggtitle(paste("Barplot: Absolute count of site dependencies\n", parametersTitle))
-  gp = ggplot(rawData, aes(x=dependency, fill = partition)) + stat_bin(aes(y = ..count..), binwidth=1, geom="bar", position = "identity") + xlab("dependencies") + facet_wrap(~tree, ncol = 3) +  ggplotTheme + ggplotTitle
+  gp = ggplot(cumData, aes(x=dependency, y=count, fill=partition)) + geom_bar(stat="identity") + geom_text(data = labelData, aes(label = count, color=partition), hjust=0, vjust=0) + xlab("dependencies") + facet_wrap(~tree, ncol = 3) +  ggplotTheme + ggplotTitle
   ggsave(file=paste(graphFileName, " absolute site dependencies", ".pdf" , sep = ""), plot = gp, w=10, h=10)
 
   ggplotTitle = ggtitle(paste("Step: Cumulated count of site dependencies\n", parametersTitle))
-  gp = ggplot(rawData, aes(x=dependency, color=partition)) + geom_step(aes(y=..y..),stat="ecdf") + xlab("dependencies") + facet_wrap(~tree, ncol = 3) + ylab("count") + ggplotTheme + ggplotTitle
+  gp = ggplot(cumData, aes(x=dependency, y=proportion, color=partition)) + geom_step() + geom_text(data = labelData, aes(label = round(proportion, 2), color=partition), hjust=0, vjust=0) + xlab("dependencies") + facet_wrap(~tree, ncol = 3) + ylab("count") + ggplotTheme + ggplotTitle
   ggsave(file=paste(graphFileName, " cumulated site dependencies", ".pdf" , sep = ""), plot = gp, w=10, h=10)
-  
   
 }
