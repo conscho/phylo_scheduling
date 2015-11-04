4ae9ca22ec94432473c1c66454e3adea66e46b9f
Constantin Scholl <constantin@scholl.im>
2015-11-04 11:36:31 +0100

    Minor

--- sorting.R
+++ sorting.R
@@ -32,13 +32,13 @@ for (parameter.file in files) {
     xlab("Sorting technique") +
     #geom_blank(aes(y=graph_points*1.1), position = "stack") + ## Dirty hack to increase upper boundary of y-axis 
     #geom_rect(data = subset(sumData, op_optimized == min(sumData$op_optimized)), aes(xmin = -Inf, ymin = -Inf, xmax = +Inf, ymax = Inf), fill = "green", alpha = 30/100) + 
-    geom_bar(stat = "identity") + 
+    geom_bar(aes(fill=partition), stat = "identity", position = "stack") + 
     #geom_text(aes(label=graph_points, bin, graph_points), position="stack", vjust = +1, size=2) + 
     #geom_text(aes(0, op_optimized, label=paste("largest bin:\n", "operations: ", op_optimized, "\n", "savings: ", savings, "%", "\n", "splits: ", splits, sep=""), group=NULL), data=sumData, vjust=-0.3, hjust=0.1/max(combData$bin), color = "red", size=3) + 
     #geom_line(aes(x=bin, y=lower_bound), color="red", data=rawData) +
-    facet_wrap(~partition, ncol = 1, scales = "free_y") + 
+    #facet_wrap(~partition, ncol = 1, scales = "free_y") + 
     ggplotTheme + ggplotTitle + ggplotRotateLabel
-  ggsave(file=paste(graphFileName, " sorting", ".pdf" , sep = ""), plot = gp, w=10, h=15)
+  ggsave(file=paste(graphFileName, " sorting", ".pdf" , sep = ""), plot = gp, w=10, h=10)
   
   
   
